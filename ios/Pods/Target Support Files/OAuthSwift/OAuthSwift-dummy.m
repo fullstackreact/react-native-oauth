0a33d526b1c61efd432cd7449970ee706c617983
Ari Lerner <arilerner@mac.com>
2016-10-05 22:37:50 -0700

    Added pods

--- ios/Pods/Target Support Files/OAuthSwift/OAuthSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_OAuthSwift : NSObject
-@end
-@implementation PodsDummy_OAuthSwift
-@end
--- ios/Pods/Target Support Files/OAuthSwift/OAuthSwift-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double OAuthSwiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char OAuthSwiftVersionString[];
-
