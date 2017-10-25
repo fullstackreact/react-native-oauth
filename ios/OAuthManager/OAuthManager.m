69258805797873c3289a39e9e6d1c75d14bc3ff2
stevepodell <steve@podell.com>
2017-10-25 10:59:46 -0700

    Added https://github.com/fullstackreact/react-native-oauth/pull/171 and my NSString *clientID fix, which might not be as good as https://github.com/fullstackreact/react-native-oauth/blob/2f8c8d1483526bbc8a6ca72183c6d11a71538ad3/ios/OAuthManager/OAuthManager.m

--- ios/OAuthManager/OAuthManager.m
+++ ios/OAuthManager/OAuthManager.m
@@ -304,6 +304,8 @@ RCT_EXPORT_METHOD(authorize:(NSString *)providerName
     
     DCTAuthAccount *existingAccount = [manager accountForProvider:providerName];
     NSString *clientID = ((DCTOAuth2Credential *) existingAccount).clientID;
+        NSString *clientID = ([providerName isEqualToString:@"google"]) ? ((DCTOAuth2Credential *) existingAccount).clientID : (NSString *)nil;
+
     if (([providerName isEqualToString:@"google"] && existingAccount && clientID != nil)
         || (![providerName isEqualToString:@"google"] && existingAccount != nil)) {
         if ([existingAccount isAuthorized]) {
