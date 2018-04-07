02c6c1da2d12a53a35e7ac2c629351aeebd7eec6
Bradley Bain <brad_bain@live.com>
2018-04-07 11:38:26 -0700

    Fix duplicate RCTMethodInfo import (https://github.com/facebook/react-native/issues/15775) (https://github.com/yonahforst/react-native-permissions/issues/137)

--- ios/OAuthManager/OAuthManager.h
+++ ios/OAuthManager/OAuthManager.h
@@ -7,10 +7,10 @@
 
 #import <Foundation/Foundation.h>
 
-#if __has_include("RCTBridgeModule.h")
-    #import "RCTBridgeModule.h"
+#if __has_include(<React/RCTBridgeModule.h>)
+  #import <React/RCTBridgeModule.h>
 #else
-    #import <React/RCTBridgeModule.h>
+  #import "RCTBridgeModule.h"
 #endif
 
 #if __has_include("RCTLinkingManager.h")
