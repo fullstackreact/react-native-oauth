e72cc08b11cc24c4547bb38da007784ef02e6cb3
Thức <duythucbk@gmail.com>
2019-03-13 11:53:42 +0700

    user agent issue when use google log in on android

--- android/src/main/java/io/fullstack/oauth/OAuthManagerDialogFragment.java
+++ android/src/main/java/io/fullstack/oauth/OAuthManagerDialogFragment.java
@@ -109,6 +109,7 @@ public class OAuthManagerDialogFragment extends DialogFragment implements Advanc
         mWebView.setVisibility(View.VISIBLE);
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setUserAgentString("Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19");
 
 
         LayoutParams layoutParams = this.getFullscreenLayoutParams(context);
